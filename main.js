39873d28260a4d57e89639ee6bafaff05b22e48c
Tatiana Limache Quispe <tati34che@gmail.com>
2016-12-22 07:56:19 -0500

    Validacion en vacio

--- main.js
+++ main.js
@@ -7,20 +7,28 @@ function calcular()
 	
 	var suma=parseInt(numero1)+parseInt(numero2);
 
-
-	if (numero1 == 10 || numero2 == 10 ) 
+	if (numero1 == "" || numero2 == "" ) 
+	{
+		salida.innerHTML = "--";
+		document.getElementById("mensajes").innerHTML = '<div class="alert alert-danger">Valores incorrectos</div>';
+		//retorna 1
+	}
+	else if (numero1 == 10 || numero2 == 10 ) 
 	{
 		salida.innerHTML = "1";
+		document.getElementById("mensajes").innerHTML = '<div class="alert alert-danger">Escribiste un 10</div>';
 		//retorna 1
 	}
 	else if	(suma == 10)
 	{
 		salida.innerHTML = "1";
 		//retorna 1
+		document.getElementById("mensajes").innerHTML = '<div class="alert alert-danger">Suma es 10</div>';
 	}
 	else 
 	{
 		salida.innerHTML = "0";
+		document.getElementById("mensajes").innerHTML = '<div class="alert alert-danger"></div>';
 		//retorna 0
 	}
 
